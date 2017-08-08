475a18ac86318941d94218e18ec5f1526ac45a5c
Daniel Bausher-Belton <dbausher@mit.edu>
2017-08-08 19:20:15 +0900

    Made fast increasing subsequence

--- Algorithms/LongestIncreasingSubsequence.py
+++ Algorithms/LongestIncreasingSubsequence.py
@@ -3,27 +3,34 @@
 import fileinput
 import collections
 
-def LISS(l, upper):
-    if(len(l) == 0):
-        return 0
-    if((tuple(l), upper) in memo):
-        return memo[(tuple(l), upper)]
-    curr = l[len(l) - 1]
-    if(curr < upper):
-        value = max(LISS(l[:len(l) - 1],curr) + 1,LISS(l[:len(l) - 1], upper))
-        memo[(tuple(l), upper)] = value
-        return value
-    else:
-        return LISS(l[:len(l) - 1], upper)
+def LISS(end):
+    if(end in memo):
+        return memo[end]
+    curr = numberList[end]
+    value = 1
+    for j in reversed(range(end)):
+        if(numberList[j] < curr):
+            jvalue = LISS(j) + 1
+            if(jvalue > value):
+                value = jvalue
+    memo[end] = value
+    return value
 
 inputLines = fileinput.input()
 
 testCases = int(inputLines.readline())
 
 for l in range(testCases):
-    listLength = inputLines.readline()
+    listLength = int(inputLines.readline())
     numberList = list(map(int,inputLines.readline().strip().split()))
-    memo = collections.defaultdict(list)
-    memo[((), 1001)].append((1001,0))
+    memo = {}
+    memo[0] = 1
 
-    print(LISS(numberList,1001))
+    length = 0
+
+    for i in range(listLength):
+        ival = LISS(i)
+        if (ival > length):
+            length = ival 
+
+    print(length)
